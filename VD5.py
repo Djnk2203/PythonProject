32ee6bfd16a16a507075457819465f143b8e2612
Djnk2203 <131732391+Djnk2203@users.noreply.github.com>
2024-03-04 15:29:11 +0700

    up

---
+++ VD5.py
@@ -0,0 +1,6 @@
+#cau lenh for
+n = 10
+myNumbers = range(n)
+# In cac so le tu 1 ..n
+for i in range(1, n, 2):
+    print(i)
---
+++ VD6.py
@@ -0,0 +1,7 @@
+#Mot may ATM cos cac loai tien 100,20 va 5 USD.Mot nguoi can rut so tien m.
+##a, Tim phuong an rut tot nhat (co it to tien nhat)
+###b, Liet ke tat ca cac cach rut duoc so tien m. co tat ca bao nhieu cach
+
+m= 225
+
+if(m > 100):
